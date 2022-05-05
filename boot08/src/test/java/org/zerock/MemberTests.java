bb016e5100c488413bbb96be924c167167f72da0
HSKim <bfpaul@daum.net>
2022-05-05 20:26:57 +0900

    boot08
    Spring Security

---
+++ boot08/src/test/java/org/zerock/MemberTests.java
@@ -0,0 +1,81 @@
+package org.zerock;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Member;
+import org.zerock.domain.MemberRole;
+import org.zerock.persistence.MemberRepository;
+
+import lombok.extern.java.Log;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log
+@Commit
+public class MemberTests {
+
+	@Autowired
+	PasswordEncoder pwEncoder;
+	
+	@Autowired
+	private MemberRepository repo;
+	
+	@Test
+	public void testInsert() {
+		
+		for(int i = 0; i <= 100; i++) {
+			
+			Member member = new Member();
+			member.setUid("user"+i);
+			member.setUpw("pw"+i);
+			member.setUname("사용자"+i);
+			
+			MemberRole role = new MemberRole();
+			
+			if(i <= 80) {
+				role.setRoleName("Basic");
+			} else if (i <= 90) {
+				role.setRoleName("Manager");
+			} else {
+				role.setRoleName("Admin");
+			}
+			
+			member.setRoles(Arrays.asList(role));
+			
+			repo.save(member);
+		}
+	}
+	
+	@Test
+	public void testRead() {
+		Optional<Member> result = repo.findById("user85");
+		
+		result.ifPresent(member -> log.info("member" + member));
+	}
+	
+	@Test
+	public void testUpdateOldMember() {
+		List<String> ids = new ArrayList<>();
+		
+		for(int i=0; i <= 100; i++) {
+			ids.add("user"+i);
+		}
+		
+		repo.findAllById(ids).forEach(member -> {
+			member.setUpw(pwEncoder.encode(member.getUpw()));
+			//member.setUpw("pw" + member.getUid().substring(4));
+			repo.save(member);
+		});
+	}
+}
